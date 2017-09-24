4ec91eace0734377dbd9eea95fd93891a8fbbc67
John Thompson <john@springframework.guru>
2017-09-24 14:06:54 -0400

    adding bootstrap for data init

---
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,47 @@
+package guru.springfamework.bootstrap;
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRespository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jt on 9/24/17.
+ */
+@Component
+public class Bootstrap implements CommandLineRunner{
+
+    private CategoryRespository categoryRespository;
+
+    public Bootstrap(CategoryRespository categoryRespository) {
+        this.categoryRespository = categoryRespository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRespository.save(fruits);
+        categoryRespository.save(dried);
+        categoryRespository.save(fresh);
+        categoryRespository.save(exotic);
+        categoryRespository.save(nuts);
+
+
+        System.out.println("Data Loaded = " + categoryRespository.count() );
+
+    }
+}

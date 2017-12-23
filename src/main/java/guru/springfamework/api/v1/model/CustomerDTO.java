d91ef04df5022c794266282bca43e1f1132e5a8f
John Thompson <john@springframework.guru>
2017-12-23 10:41:58 -0500

    customized Customer Endpoint doc

--- src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CustomerDTO.java
@@ -1,6 +1,7 @@
 package guru.springfamework.api.v1.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CustomerDTO {
 
+    @ApiModelProperty(value = "This is the first name", required = true)
     private String firstname;
+
+    @ApiModelProperty(required = true )
     private String lastname;
 
     @JsonProperty("customer_url")
--- src/main/java/guru/springfamework/controllers/v1/CustomerController.java
+++ src/main/java/guru/springfamework/controllers/v1/CustomerController.java
@@ -3,12 +3,15 @@ package guru.springfamework.controllers.v1;
 import guru.springfamework.api.v1.model.CustomerDTO;
 import guru.springfamework.api.v1.model.CustomerListDTO;
 import guru.springfamework.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 9/27/17.
  */
+@Api(description = "This is my Customer Controller")
 @RestController
 @RequestMapping(CustomerController.BASE_URL)
 public class CustomerController {
@@ -21,6 +24,7 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "This will get a list of customers.", notes = "These are some notes about the API.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public CustomerListDTO getListOfCustomers(){

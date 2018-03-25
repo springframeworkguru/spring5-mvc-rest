e5bd12fab1bd80818aad60e11fceac4beab5b863
springframeworkguru <john@springframework.guru>
2018-03-25 19:43:25 +0530

    Removed explicit @ComponentScan

--- src/main/java/guru/springfamework/Spring5MvcRestApplication.java
+++ src/main/java/guru/springfamework/Spring5MvcRestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("guru.springfamework.api.v1.mapper, guru.springfamework.bootstrap, guru.springfamework.controllers.v1, guru.springfamework.domain, guru.springfamework.repositories, guru.springfamework.services ")
 public class Spring5MvcRestApplication {
 
 	public static void main(String[] args) {

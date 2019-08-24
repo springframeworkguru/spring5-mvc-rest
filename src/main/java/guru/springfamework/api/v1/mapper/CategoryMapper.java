9eefdef54772f9aab92409af609e57b0f5275137
Cesar Nicolas Cabo <cesar.nicolas.cabo@gmail.com>
2019-08-24 11:48:18 -0300

    Data annotation to CategoryDTO added

---
+++ src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,4 @@
+package guru.springfamework.api.v1.mapper;
+
+public interface CategoryMapper {
+}
--- src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
+++ src/main/java/guru/springfamework/api/v1/model/CategoryDTO.java
@@ -1,8 +1,11 @@
 package guru.springfamework.api.v1.model;
 
+import lombok.Data;
+
 /**
  * Created by ccabo on 8/24/19.
  */
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;

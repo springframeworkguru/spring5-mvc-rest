18465645637ab9e30fec86b3a62c0a77af2c1354
springframeworguru <john@springframework.guru>
2020-06-14 16:35:14 +0530

    fixed missing import

--- src/main/java/guru/springfamework/services/CategoryServiceImpl.java
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package guru.springfamework.services;
 import guru.springfamework.api.v1.mapper.CategoryMapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
